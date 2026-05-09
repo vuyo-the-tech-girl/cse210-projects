4aa157d0af752a311764465ed968b5997ee69a03
vuyo-the-tech-girl <vfaleni@byupathway.edu>
2026-05-09 19:46:30 +0200

    assignment Ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guess = 0;
+        int guessCount = 0;
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {guessCount} tries!");
+            }
+        }
     }
 }
