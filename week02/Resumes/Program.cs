c394c966ad5fe32050cef0eff90803ac2dbd9da7
vuyo-the-tech-girl <vfaleni@byupathway.edu>
2026-05-14 21:01:29 +0200

    W02 Learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Pintrest";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cloud Engineer";
+        job2._company = "Spotify";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Vuyo Faleni";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
